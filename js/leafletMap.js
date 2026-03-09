6a86aab555e11af4347ffa7a9bfda7a5a4c014f6
HN <nguye2cy@mail.uc.edu>
2026-03-09 17:46:43 -0400

    Feat: Implement set of colors to filter by each category and implement toggles for different map background

--- js/leafletMap.js
+++ js/leafletMap.js
@@ -44,6 +44,24 @@ class LeafletMap {
       ext: 'png'
     });
 
+    vis.topo_layer = L.tileLayer(vis.topoUrl, {
+      id: 'topo',
+      attribution: vis.topoAttr,
+      ext: 'png'
+    });
+
+    vis.thOut_layer = L.tileLayer(vis.thOutUrl, {
+      id: 'thOut',
+      attribution: vis.thOutAttr,
+      ext: 'png'
+    });
+
+    vis.st_layer = L.tileLayer(vis.stUrl, {
+      id: 'st',
+      attribution: vis.stAttr,
+      ext: 'png'
+    });
+
     vis.theMap = L.map('my-map', {
       center: [39.1031, -84.5120],
       zoom: 12,
@@ -52,6 +70,13 @@ class LeafletMap {
       layers: [vis.base_layer]
     });
 
+    let baseMaps = {
+      "Esri Map": vis.base_layer,
+      "Topo Map": vis.topo_layer,
+      "Thunderforest": vis.thOut_layer,
+      "Stamen Terrain": vis.st_layer
+    };
+    L.control.layers(baseMaps).addTo(vis.theMap);
     //if you stopped here, you would just have a map
 
     //initialize svg for d3 to add to map
@@ -61,6 +86,32 @@ class LeafletMap {
     vis.missingGPS = vis.data.filter(d => !d.LATITUDE || !d.LONGITUDE).length; //Count missing GPS coordinates
     d3.select("#missing-data")
       .text(`${vis.missingGPS} requests could not be mapped because they have missing coordinates`);
+
+    //Color Scales
+
+    vis.responseScale = d3.scaleSequential() //Response time
+      .domain(d3.extent(vis.data, d => d.response_days))
+      .interpolator(d3.interpolateOrRd);
+    vis.neighborhoodScale = d3.scaleOrdinal(d3.schemeCategory10);//Neighborhood
+    vis.priorityScale = d3.scaleOrdinal()//Priority
+      .domain(["LOW","MEDIUM","HIGH"])
+      .range(["green","orange","red"]);
+    vis.departmentScale = d3.scaleOrdinal(d3.schemeTableau10);//Department
+
+    vis.colorMode = "response";//Default color mode
+
+    //Use the colors
+    d3.select("#color-select").on("change", function() {
+
+      vis.colorMode = this.value;
+
+      vis.Dots
+        .transition()
+        .duration(400)
+        .attr("fill", d => vis.getColor(d));
+
+    });
+    
     //these are the city locations, displayed as a set of dots 
     vis.Dots = vis.svg.selectAll('circle')
                     .data(vis.data.filter(d => d.LATITUDE && d.LONGITUDE))
@@ -135,11 +186,35 @@ class LeafletMap {
     vis.Dots
       .attr("cx", d => vis.theMap.latLngToLayerPoint([d.LATITUDE,d.LONGITUDE]).x)
       .attr("cy", d => vis.theMap.latLngToLayerPoint([d.LATITUDE,d.LONGITUDE]).y)
-      .attr("fill", "steelblue")  //---- TO DO- color by magnitude 
+      .attr("fill", d => vis.getColor(d))  //---- TO DO- color by magnitude 
       .attr("r", 3) ; 
 
   }
 
+  getColor(d) {
+    let vis = this;
+
+    if (vis.colorMode === "response") {
+      if (!d.response_days || isNaN(d.response_days)) {
+        return "steelblue";
+      }
+      return vis.responseScale(d.response_days);
+    }
+
+    if (vis.colorMode === "neighborhood") {
+      return vis.neighborhoodScale(d.NEIGHBORHOOD);
+    }
+
+    if (vis.colorMode === "priority") {
+      return vis.priorityScale(d.PRIORITY);
+    }
+
+    if (vis.colorMode === "department") {
+      return vis.departmentScale(d.DEPT_NAME);
+    }
+
+    return "steelblue";
+  }
 
   renderVis() {
     let vis = this;
